0d993e82fdfdef90ed10482f5271fb89cf8caa99
unknown <syedsasdc.ams@gmail.com>
2023-12-07 22:04:07 +0530

    adding project

---
+++ script.js
@@ -0,0 +1,24 @@
+let moon=document.querySelector('#moon')
+let sun=document.querySelector('#sun')
+let ham=document.querySelector('#ham')
+
+moon.addEventListener('click',function(){
+    document.body.classList.toggle('dark-theme')
+
+        // if(document.body.classList == .dark-theme){
+        //     <i class="ri-sun-line" id="sun"></i>
+        // }
+        // else{
+        //     <i class="ri-moon-fill" id="moon"></i>
+        // }
+    
+})
+
+ham.addEventListener('click',function(){
+{
+    ham.classList.toggle('items')
+
+    }
+})
+
+    
